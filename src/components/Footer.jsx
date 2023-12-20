b2894182fdfe2f11fc000d2161a49c96a6808ffb
unchangedraman <unchangedraman@gmail.com>
2023-12-21 00:23:27 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,77 @@
+import {
+    Box,
+    Button,
+    Heading,
+    HStack,
+    Input,
+    Stack,
+    Text,
+    VStack,
+  } from '@chakra-ui/react';
+  import React from 'react';
+  import { AiOutlineSend } from 'react-icons/ai'; //blackAlpha.900
+  
+  const Footer = () => {
+    return (
+      <Box bgColor={'#1A365D'} minH={'40px'} p="16" color={'white'}>
+        <Stack direction={['column', 'row']}>
+          <VStack alignItems={'stretch'} w={'full'} >
+            <Heading
+              size="md"
+              textTransform={'uppercase'}
+              textAlign={['center', 'left']}
+            
+            >
+              Subscribe to get updates
+            </Heading>
+            <HStack borderBottom={'2px solid white'} py="2">
+              <Input
+                placeholder="Enter Email Here..."
+                border={'none'}
+                borderRadius="none"
+                outline={'none'}
+                focusBorderColor="none"
+              />
+              <Button
+                p={'0'}
+                colorScheme={'purple'}
+                variant={'ghost'}
+                borderRadius={'0 20px 20px 0'}
+              >
+                <AiOutlineSend size={20} />
+              </Button>
+            </HStack>
+          </VStack>
+  
+          <VStack
+            w={'full'}
+            borderLeft={['none', '1px solid white']}
+            borderRight={['none', '1px solid white']}
+          >
+            <Heading textTransform={'uppercase'} textAlign={'center'}>
+              VIDEO HUB
+            </Heading>
+            <Text>EveryThing is reserve</Text>
+          </VStack>
+  
+          <VStack w={'full'} borderRight={['none', '1px solid white']}>
+            <Heading size={'md'} textTransform={'uppercase'}>
+              Social Media
+            </Heading>
+            <Button variant={'link'} colorScheme={'white'}>
+                Youtube
+            </Button>
+            <Button variant={'link'} colorScheme={'white'}>
+                Instagram
+            </Button>
+  
+            <Button variant={'link'} colorScheme={'white'}>
+                FaceBook
+            </Button>
+          </VStack>
+        </Stack>
+      </Box>
+    );
+  };
+  
+  export default Footer;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Videos = () => {
+  return (
+    <div>Yaha pe bhi aayega kuchh</div>
+  )
+}
+
+export default Videos
